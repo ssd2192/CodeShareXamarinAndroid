561c309817ef5cd5c2d2f7ea873f3d4d7e57af85
Sandeep <ssd2192@gmail.com>
2019-08-10 13:02:55 -0400

    Add Jpeg Image ScreenShot

--- CodeShare/AddCodeActivity.cs
+++ CodeShare/AddCodeActivity.cs
@@ -112,9 +112,10 @@ namespace CodeShare
             imageArray = System.IO.File.ReadAllBytes(file.Path);
             bitmap = BitmapFactory.DecodeByteArray(imageArray, 0, imageArray.Length);
             imageView_screen.SetImageBitmap(bitmap);
+
             using (var stream = new MemoryStream())
             {
-                bitmap.Compress(Bitmap.CompressFormat.Png, 0, stream);
+                bitmap.Compress(Bitmap.CompressFormat.Jpeg, 0, stream);
 
                 var bytes = stream.ToArray();
                 var str = Convert.ToBase64String(bytes);
--- CodeShare/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ CodeShare/obj/Debug/90/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "5e078e5d-206d-4252-9cc0-61d6bb0bf61b",
+		"XAMARIN_BUILD_ID", "e597c341-9dcc-4afb-a1da-867756d985c6",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
